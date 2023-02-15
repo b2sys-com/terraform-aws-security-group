f88617748b3093e8aacd2732ffa59f028911f7c4
diegosorace <dsorace@prismamp.com>
2023-02-15 15:59:30 -0300

    update

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "security_group_arn" {
   description = "The ARN of the security group"
-  value       = try(aws_security_group.this[0].arn, aws_security_group.this_name_prefix[0].arn, "")
+  value       = try(aws_security_group.this.arn, aws_security_group.this_name_prefix.arn, "")
 }
 
 output "security_group_id" {
   description = "The ID of the security group"
-  value       = try(aws_security_group.this[0].id, aws_security_group.this_name_prefix[0].id, "")
+  value       = try(aws_security_group.this.id, aws_security_group.this_name_prefix.id, "")
 }
